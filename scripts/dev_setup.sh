be6bfc06fbb4bbf5160bae1531dee1811bdc04a5
Aryan Yadav <ayrawwat@gmail.com>
2025-07-01 19:36:20 +0000

    some changes

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -2,25 +2,27 @@
 exec > /var/log/user_data.log 2>&1
 set -x
 
+# Update OS
 yum update -y
 
-# Install Java 21
-yum install -y java-21-amazon-corretto
+# Install Java 17 (compatible & available)
+amazon-linux-extras enable corretto17
+yum install -y java-17-amazon-corretto
 
-# Install Maven
+# Install Maven via yum (skip zip fail)
 yum install -y maven git
 
-# Clone GitHub repo
+# Clone your GitHub repo
 cd /home/ec2-user
 git clone ${repo_url} app
 cd app
 
-# Build and run the application
+# Build with Maven
 mvn clean install
 
-# Run the app on port 80
+# Run app on port 80
 nohup java -jar target/*.jar --server.port=80 &
 
-# Shutdown after 1 hour to save cost
+# Shutdown instance after 1 hour
 shutdown -h +60
 
